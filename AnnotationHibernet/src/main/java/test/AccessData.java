7e526a4bf13951b2892f161076b0ad9f9216096a
Shubhangi-Shelke <shubhangishelke9272@gmail.com>
2026-04-01 17:37:29 +0530

    Annotation_@

---
+++ AnnotationHibernet/src/main/java/test/AccessData.java
@@ -0,0 +1,44 @@
+package test;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class AccessData {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+Configuration conf =new Configuration();
+		
+		conf.configure("hibernate.cfg.xml");
+		
+		// step-2
+		
+		SessionFactory factory =conf.buildSessionFactory();
+		
+		// step-3
+		Session session=factory.openSession();
+		
+		// step -4
+		Transaction tx=session.beginTransaction();
+		
+		// create persistance object
+		
+		 Employee emp= new  Employee ();
+		 
+		 emp.setId(101);
+		 emp.setName("riya");
+		 emp.setSalary(35000);
+		session.save(emp);
+		tx.commit(); 
+		session.close();
+		factory.close();
+		
+		System.out.println("Details Added Successfully...");
+		
+		
+	}
+
+}
---
+++ AnnotationHibernet/src/main/java/test/Employee.java
@@ -0,0 +1,42 @@
+package test;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="empno")
+public class Employee {
+
+	@Id
+	private int id;
+	private String name;
+	private double salary;
+	
+	
+	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+}
